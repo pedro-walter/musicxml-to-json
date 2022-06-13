680f1a95bd5af29334cfa76c9c4853dc45020b1e
Pedro Walter <pedrohardware@gmail.com>
2022-06-13 15:08:45 -0300

    Incluindo timestamp em segundos

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Testado no python 3.9.5
 Por enquanto eu fiz um esquema assim:
 
 [
-    {'beat': XXX, 'note': YZ},
-    {'beat': XXX, 'note': YZ},
+    {'beat': XXX, 'note': YZ, 'timestamp': W.WW},
+    {'beat': XXX, 'note': YZ, 'timestamp': W.WW},
     {...},
     ...
 ]
@@ -14,3 +14,4 @@ Sendo:
 XXX: em qual batida a nota acontece
 Y: Oitava a que a nota pertence
 Z: A nota em si (C=Dó, D=Ré, ....)
+W.WW: em quantos segundos da música a nota é tocada, basicamente (BPM / 60 * batida)
--- parse.py
+++ parse.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+import fractions
 import json
 import xml.etree.ElementTree as ET
 
@@ -9,13 +11,16 @@ notes = root.findall('part/measure/note')
 voice_current_beat = [0 for i in range(16)]
 music = []
 
+seconds_per_beat = fractions.Fraction(1000,1125)
+
 for note in notes:
     voice = int(note.find('voice').text)
     duration = int(note.find('duration').text)
     if note.find('pitch'):
         music.append({
             'beat': voice_current_beat[voice],
-            'note': note.find('pitch/octave').text + note.find('pitch/step').text
+            'note': note.find('pitch/octave').text + note.find('pitch/step').text,
+            'timestamp': timedelta(seconds=float(seconds_per_beat * voice_current_beat[voice])).total_seconds()
         })
     voice_current_beat[voice] += duration
 
