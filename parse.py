c89f6e0075859bc82ea380380bdad47f9f99264e
Pedro Walter <pedrohardware@gmail.com>
2022-06-19 21:58:21 -0300

    Fixing beat timing

--- parse.py
+++ parse.py
@@ -1,29 +1,50 @@
 from datetime import timedelta
 import fractions
 import json
+from typing import Counter
 import xml.etree.ElementTree as ET
+
+import pdb
 
 tree = ET.parse('gymnopdie.musicxml')
 root = tree.getroot()
 
-notes = root.findall('part/measure/note')
+beats_per_measure = int(root.find('part/measure/attributes/time/beats').text)
 
-voice_current_beat = [0 for i in range(16)]
 music = []
 
 seconds_per_beat = fractions.Fraction(1000,1125)
 
-for note in notes:
-    voice = int(note.find('voice').text)
-    duration = int(note.find('duration').text)
-    if note.find('pitch'):
-        music.append({
-            'beat': voice_current_beat[voice],
-            'note': note.find('pitch/octave').text + note.find('pitch/step').text,
-            'timestamp': timedelta(seconds=float(seconds_per_beat * voice_current_beat[voice])).total_seconds()
-        })
-    voice_current_beat[voice] += duration
+current_measure = 0
+last_loop_beat = None
+
+for measure in root.findall('part/measure'):
+    voice_current_beat = Counter()
+    for note in measure.findall('note'):
+        voice = int(note.find('voice').text)
+        duration = int(note.find('duration').text)
+        if note.find('pitch'):
+            is_chord = len(note.findall('chord')) > 0
+            if is_chord:
+                this_loop_beat = last_loop_beat
+            else:
+                this_loop_beat = current_measure * beats_per_measure + voice_current_beat[voice]
+                last_loop_beat = this_loop_beat
+                voice_current_beat[voice] += duration
+            to_append = {
+                'beat': this_loop_beat,
+                'duration': duration,
+                'voice': voice,
+                'note': note.find('pitch/octave').text + note.find('pitch/step').text,
+                'timestamp': timedelta(seconds=float(seconds_per_beat * this_loop_beat)).total_seconds()
+            }
+            print(to_append)
+            music.append(to_append)
+        else:
+            voice_current_beat[voice] += duration
+        ET.dump(note)
+        print(voice_current_beat)
+    current_measure += 1
 
 music.sort(key=lambda x: x['beat'])
-
 json.dump(music, open('gymnopdie.json', 'w'))
